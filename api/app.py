e972e5ba184c111a156788c13eb229a083d3dd79
Anton Yefremov <anthyef@gmail.com>
2019-12-25 02:50:07 +0700

    Bind front and api together
    Signed-off-by: Anton Yefremov <anthyef@gmail.com>

--- api/app.py
+++ api/app.py
@@ -2,11 +2,10 @@ import logging
 from aiohttp import web
 
 async def hello(request):
-    return web.Response(text="Hello, bangkok")
-
+    return web.Response(text="Hello, Bangkok")
 
 app = web.Application()
-app.add_routes([web.get('/', hello)])
+app.add_routes([web.get('/_api/', hello)])
 
 logging.basicConfig(level=logging.DEBUG)
 
--- front/src/App.js
+++ front/src/App.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [apiText, setApiText] = useState('');
+  
+  useEffect( () => {
+    fetch("/_api/").then(resp => resp.text()).then(setApiText);
+  }, [apiText]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>The API bravely responds:</h1>
+        <p>{apiText}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
