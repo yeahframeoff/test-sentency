4f1edd120bcfe2a8f97deca230bb1f71421c681d
Anton Yefremov <anthyef@gmail.com>
2019-12-25 00:34:57 +0700

    Enable live reload for backend
    Signed-off-by: Anton Yefremov <anthyef@gmail.com>

--- api/app.py
+++ api/app.py
@@ -1,10 +1,14 @@
+import logging
 from aiohttp import web
 
 async def hello(request):
-    return web.Response(text="Hello, api")
+    return web.Response(text="Hello, bangkok")
 
 
 app = web.Application()
 app.add_routes([web.get('/', hello)])
 
-web.run_app(app)
+logging.basicConfig(level=logging.DEBUG)
+
+if __name__ == "__main__":
+    web.run_app(app)
